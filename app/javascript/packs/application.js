2048e7a2d326d7e16b3745f16ef0be46c0f71ef6
tinaxycheng <tinaxycheng@gmail.com>
2019-10-09 12:24:07 +0800

    changetofixthecss

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 import 'bootstrap'
-import '../stylesheets/application'
+
 document.addEventListener("turbolinks:load", () => {
   $('[data-toggle="tooltip"]').tooltip()
 })
